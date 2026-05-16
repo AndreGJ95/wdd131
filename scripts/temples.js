796a304e00d4a247847934a75025ba7d9d0fe8b3
AndreGJ95 <andredavidgj95@gmail.com>
2026-05-16 00:27:01 -0500

    attempt4.0

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,4 +1,4 @@
-// FOOTER DATES
+
 
 const year = document.querySelector("#currentyear");
 const lastModified = document.querySelector("#lastModified");
@@ -8,7 +8,7 @@ year.textContent = new Date().getFullYear();
 lastModified.textContent =
 `Last Modification: ${document.lastModified}`;
 
-// HAMBURGER MENU
+
 
 const hamButton = document.querySelector("#menu");
 const navigation = document.querySelector("nav");
